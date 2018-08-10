ae03aa547d4d09712ded14628d14a43653d6c173
clement <cgng16@gmail.com>
2018-08-09 19:10:24 -0700

    add some backend stuff and bcrypt

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json()); // need to use bodyParser to parse JSON objects
+app.use(cors());
 
 const database = {
   users: [
@@ -22,6 +25,13 @@ const database = {
       entries: 0,
       joined: new Date()
     }
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'clement@boss.com'
+    }
   ]
 }
 
@@ -34,21 +44,34 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+  // Load hash from your password DB.
+  bcrypt.compare("boss", '$2a$10$ZLCwHW9SBkIyAdsWaeH76Or1zfDNsD.5lV65aB.fImbRJ2Y8xbfwu', function(err, res) {
+      // res == true
+      console.log('first guess', res);
+  });
+  bcrypt.compare("veggies", '$2a$10$ZLCwHW9SBkIyAdsWaeH76Or1zfDNsD.5lV65aB.fImbRJ2Y8xbfwu', function(err, res) {
+      // res = false
+      console.log('second guess', res);
+  });
+
   if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-    res.json('success!');
+    res.json(database.users[0]);
   } else {
     res.status(400).json('Wrong email or password')
   }
-  res.json('signed in');
+  // res.json('signed in');
 })
 
 app.post('/signup', (req, res) => {
   const { name, email, password} = req.body;
+  bcrypt.hash(password, null, null, function(err, hash) {
+      // Store hash in your password DB.
+      console.log(hash);
+  });
   database.users.push({
     id: '125',
     name: name,
     email: email,
-    password: password,
     entries: 0,
     joined: new Date()
   })
@@ -89,8 +112,9 @@ app.put('/image', (req, res) => {
   }
 })
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000');
+
+app.listen(3001, () => {
+  console.log('app is running on port 3001');
 })
 
 /*
@@ -101,5 +125,45 @@ app.listen(3000, () => {
 /image - PUT request = return user
 
 res.json('signed in') will return a JSON string of "signed in"
+
+always use HTTPS when sending sensitive info from frontend to backend in a POST body
+
+*/
+
+/*
+
+$ npm install bcrypt
+/*
+* You can copy and run the code below to play around with bcrypt
+* However this is for demonstration purposes only. Use these concepts
+* to adapt to your own project needs.
+
+
+import bcrypt from'bcrypt'
+const saltRounds = 10 // increase this if you want more iterations
+const userPassword = 'supersecretpassword'
+const randomPassword = 'fakepassword'
+
+const storeUserPassword = (password, salt) =>
+  bcrypt.hash(password, salt).then(storeHashInDatabase)
+
+const storeHashInDatabase = (hash) => {
+   // Store the hash in your password DB
+   return hash // For now we are returning the hash for testing at the bottom
+}
+
+// Returns true if user password is correct, returns false otherwise
+const checkUserPassword = (enteredPassword, storedPasswordHash) =>
+  bcrypt.compare(enteredPassword, storedPasswordHash)
+
+
+// This is for demonstration purposes only.
+storeUserPassword(userPassword, saltRounds)
+  .then(hash =>
+    // change param userPassword to randomPassword to get false
+    checkUserPassword(userPassword, hash)
+  )
+  .then(console.log)
+  .catch(console.error)
 
 */
